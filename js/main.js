7cec32ee6f0294fff2be99fdb244fd9117c97662
Adao <m14346@mjundu.com.br>
2020-06-09 23:38:32 -0300

    Aula de Javascript

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function clicado(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //alert("Obrigado por clicar")
+}
+
+/*function soma(n1,n2){
+    return n1 + n2;
+}
+alert(soma(5,10));
+*/
+/*var d = new Date();
+alert(d.getUTCDate());*/
+
+/*for(count=0;count<=5;count++){
+    console.log(count);
+}*/
+
+/*var count = 0;
+while (count < 5){
+    console.log(count);
+    count++;
+};*/
+
+
+/*var idade = prompt("Qual a sua idade?");
+
+if(idade>18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+
+};*/
+
+//var fruta=[{nome:"maçã", cor: "vermelha"},{nome:"uva", cor: "roxa"}];
+//console.log(fruta[1].nome);
+
+//var lista=["maçã","pera","laranja"];
+//lista.push("manga");
+//lista.pop();
+//lista.lenght();
+//console.log(lista);
+
+//var nome = "Marcio";
+//var idade = "42"
+//var frase =nome + " tem "+idade +" anos";
+//alert(nome + " tem "+idade +"anos");
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Marcio","Joca"));
